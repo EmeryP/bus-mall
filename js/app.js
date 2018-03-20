741bcafda722eab142fb5bdd14df88fd76357e09
EmeryP <emeryparks09@hotmail.com>
2018-03-20 15:44:58 -0700

    printing names to array working, global vars set

--- js/app.js
+++ js/app.js
@@ -1,34 +1,43 @@
 'use strict';
 
-'use strict';
-
 // array to hold photo objects
 Products.allProducts = [];
 
 //populated with 3 random filepaths
 var viewedArray = [];
 
+// array holding previous generated indicies
+var lastIndices = [];
+
 // photo views array
-// var photoViews = [];
+var photoNames = [];
 
 // photo names array
-// var photoNames = [];
+var photoVotes = [];
 
+// click tracker
+Products.totalClicks = 0;
 
 // access the elememts from the DOM
 viewedArray[0] = document.getElementById('product-pic1'); //put viewed array into product pic1
 viewedArray[1] = document.getElementById('product-pic2');
 viewedArray[2] = document.getElementById('product-pic3');
 
+// access the section element from the DOM
+var sectionElement = document.getElementById('product-section');
+
+// access the unordered list element from the DOM
+var unorderedListElement = document.getElementById('results');
+
 // photo constructor for photo objects
 function Products(filepath, name){
   this.filepath = filepath;
   this.name = name;
-  // this.views = views;
-  // this.votes = votes;
+  this.timesDisplayed = 0;
+  this.votes = 0;
   Products.allProducts.push(this);
+  photoNames.push(this.name);
 }
-
 
 // new instances of photos
 new Products('img/bag.jpg', 'bag'); //store views, clicks, id, in separate var and place here
@@ -57,8 +66,7 @@ viewedArray[0].addEventListener('click', randomPhoto);
 viewedArray[1].addEventListener('click', randomPhoto);
 viewedArray[2].addEventListener('click', randomPhoto);
 
-var lastIndices = [];
-
+// function to run when random photo is called
 function randomPhoto(){
 
   var randomIndices = [];
@@ -70,20 +78,34 @@ function randomPhoto(){
     if(randomIndices.includes(randomIndex) || lastIndices.includes(randomIndex)) {
       i--;
     } else{
-
-      // use the randomIndex to set the src and alt attributes of the imgElement
-      viewedArray[i].src = Products.allProducts[randomIndex].filepath; //overwrite random number from array with filepath
+      // use the randomIndex to set the src and alt attributes of the imgElement in HTML
+      viewedArray[i].src = Products.allProducts[randomIndex].filepath;
       viewedArray[i].alt = Products.allProducts[randomIndex].name;
       randomIndices.push(randomIndex);
+      // Products.allProducts.timesDisplayed++;
+      // console.log(Products.allProducts.timesDisplayed);
     }
-
   }
   lastIndices = randomIndices;
-  console.log(lastIndices);
+  // console.log(lastIndices);
 }
 
+
+function handleClick(){
+
+    // use a for loop to determine which goat img was actually clicked on
+    for(var i in Goat.allGoats) {
+      if(event.target.alt === Goat.allGoats[i].name) {
+        Goat.allGoats[i].votes++;
+      }
+    }
+}
+
+
+
+
 randomPhoto();
-console.log(viewedArray);
+// console.log(viewedArray);
 
 
 // build click listener 
